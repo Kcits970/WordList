e396c93958a0793555cfb86c112cd7a1c04c602a
Taehyun Ahn <93324991+Kcits970@users.noreply.github.com>
2022-01-11 13:17:24 +0900

    Delete Launcher.java

--- Launcher.java
+++
@@ -1,8 +0,0 @@
-import frame.mainframe.MyFrame;
-import word.WordBase;
-
-public class Launcher {
-	public static void main(String[] args) {
-		javax.swing.SwingUtilities.invokeLater(() -> new MyFrame(new WordBase()));
-	}
-}
